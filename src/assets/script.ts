7c5e9622dd499ebefc0e35013c6b9e40c95a759d
james-work-account <31282758+james-work-account@users.noreply.github.com>
2024-05-20 17:42:13 +0100

    [styling] initial commit - styling

---
+++ src/assets/script.ts
@@ -0,0 +1,16 @@
+const form = document.querySelector("form");
+const toast = document.querySelector(".toast");
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  const success = true;
+
+  if (success) {
+    toast.classList.add("show");
+    // Remove the "show" class after another 5 seconds
+    setTimeout(() => {
+      toast.classList.remove("show");
+    }, 2900);
+  }
+};
