e3a175e25a94d1b8aae1998a49bb7c38e1ccd351
james-work-account <31282758+james-work-account@users.noreply.github.com>
2024-05-20 22:53:38 +0100

    validation - frontend

--- netlify/functions/validate-input/validate-input.ts
+++ netlify/functions/validate-input/validate-input.ts
@@ -1,16 +1,16 @@
 import { Handler } from "@netlify/functions";
 
 type FormParams = {
-  firstName?: string;
-  lastName?: string;
+  "first-name"?: string;
+  "last-name"?: string;
   email?: string;
   message?: string;
-  queryType?: string;
+  "query-type"?: "general-enquiry" | "support-query";
   consent?: boolean;
 };
 
 export const handler: Handler = async (event, context) => {
-  let body;
+  let body: FormParams;
   try {
     body = JSON.parse(event.body) as FormParams;
   } catch (e) {
@@ -24,12 +24,13 @@ export const handler: Handler = async (event, context) => {
   }
 
   let badFields = [];
-  if (!body.firstName) badFields.push("firstName");
-  if (!body.lastName) badFields.push("lastName");
-  if (!body.email) badFields.push("email");
+  if (!body["first-name"]) badFields.push("first-name");
+  if (!body["last-name"]) badFields.push("last-name");
+  if (!body.email) badFields.push("email-required");
+  if (body.email && body.email.length < 10) badFields.push("email-invalid"); // arbitrary check to show validation working
   if (!body.message) badFields.push("message");
-  if (!body.queryType) badFields.push("queryType");
-  if (!body.consent) badFields.push("consent");
+  if (!body["query-type"]) badFields.push("query-type-required");
+  if (!body.consent) badFields.push("consent-required");
 
   if (badFields.length > 0) {
     return {
--- src/assets/script.ts
+++ src/assets/script.ts
@@ -1,20 +1,88 @@
 const form = document.querySelector("form")!;
 const toast = document.querySelector(".toast")!;
 
+function clearErrors() {
+  form.querySelectorAll("error").forEach((field) => {
+    field.classList.remove("show");
+  });
+}
+
+function clearErrorInput(event: InputEvent) {
+  if (event.data === "") return;
+  document
+    .querySelectorAll(
+      `[data-for*=${(event.target as HTMLElement).getAttribute("name")}].error, [data-for*=${(event.target as HTMLElement).getAttribute("name")}]+.error`
+    )
+    .forEach((field) => {
+      field.classList.remove("show");
+    });
+}
+function clearErrorClick(event: MouseEvent) {
+  document
+    .querySelectorAll(
+      `[data-for*=${(event.target as HTMLElement).getAttribute("name")}].error, [data-for*=${(event.target as HTMLElement).getAttribute("name")}]+.error`
+    )
+    .forEach((field) => {
+      field.classList.remove("show");
+    });
+}
+
+form.oninput = clearErrorInput;
+
+const generalEnquiry = form.querySelector("[for=general-enquiry]")! as HTMLLabelElement;
+generalEnquiry.onclick = clearErrorClick;
+
+const supportQuery = form.querySelector("[for=support-query]")! as HTMLLabelElement;
+supportQuery.onclick = clearErrorClick;
+
+const consent = form.querySelector("[for=consent]")! as HTMLLabelElement;
+consent.onclick = clearErrorClick;
+
 form.onsubmit = async (event) => {
   event.preventDefault();
 
-  const res = await fetch("/.netlify/functions/validate-input");
-  const data = await res.json();
-  console.log("data", data);
+  clearErrors();
 
-  //   const success = true;
-
-  //   if (success) {
-  //     toast.classList.add("show");
-  //     // Remove the "show" class after another 5 seconds
-  //     setTimeout(() => {
-  //       toast.classList.remove("show");
-  //     }, 2900);
-  //   }
+  const res = await fetch("/.netlify/functions/validate-input", {
+    method: "POST",
+    body: JSON.stringify({
+      "first-name": (form.querySelector('[name="first-name"]') as HTMLInputElement)?.value,
+      "last-name": (form.querySelector('[name="last-name"]') as HTMLInputElement)?.value,
+      email: (form.querySelector('[name="email"]') as HTMLInputElement)?.value,
+      message: (form.querySelector('[name="message"]') as HTMLInputElement)?.value,
+      "query-type": (form.querySelector('[name="query-type"]:checked') as HTMLInputElement)?.value,
+      consent: (form.querySelector('[name="consent"]') as HTMLInputElement)?.checked,
+    }),
+  });
+  switch (res.status) {
+    case 204:
+      // Handle success case
+      form.reset();
+      toast.classList.add("show");
+      setTimeout(() => {
+        toast.classList.remove("show");
+      }, 2900);
+      break;
+    case 400:
+      // Handle bad request case
+      const badFields = (await res.json()).badFields;
+      const badFieldsSet = new Set(badFields);
+      console.log(badFieldsSet);
+      badFieldsSet.forEach((field) => {
+        form.querySelector(`[id="${field}"] + .error, [id="${field}"].error`).classList.add("show");
+      });
+      break;
+    case 422:
+      // Handle unprocessable entity case
+      console.error("Unprocessable entity");
+      break;
+    case 500:
+      // Handle server error case
+      console.error("Server error");
+      break;
+    default:
+      // Handle other cases
+      console.error("Unknown error", res.status);
+      break;
+  }
 };
